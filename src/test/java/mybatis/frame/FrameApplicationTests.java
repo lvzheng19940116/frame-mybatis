878289c7e9e6735cb690e44c292ad68ccf102656
lvzheng <lvzheng@samsundot.com>
2020-07-03 10:35:05 +0800

    init

---
+++ src/test/java/mybatis/frame/FrameApplicationTests.java
@@ -0,0 +1,13 @@
+package mybatis.frame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
